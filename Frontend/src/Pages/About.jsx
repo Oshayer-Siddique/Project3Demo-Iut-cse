a226d66aa26682996e99ffbc3fa4d5b560a4c686
MetheMeticien <rifat7688@gmail.com>
2023-09-24 06:23:23 +0600

    def

---
+++ Frontend/src/Pages/About.jsx
@@ -0,0 +1,182 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
+
+
+const AboutPage = () => {
+  return (
+    <div>
+      <header id="header" className="fixed-top">
+        <div className="container d-flex align-items-center">
+          <h1 className="logo me-auto"><a href="/">Mentor</a></h1>
+          <nav id="navbar" className="navbar order-last order-lg-0">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a className="active" href="/about">About</a></li>
+              <li><a href="/courses">Courses</a></li>
+              <li><a href="/trainers">Trainers</a></li>
+              <li><a href="/events">Events</a></li>
+              <li><a href="/pricing">Pricing</a></li>
+              <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
+                <ul>
+                  <li><a href="#">Drop Down 1</a></li>
+                  <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
+                    <ul>
+                      <li><a href="#">Deep Drop Down 1</a></li>
+                      <li><a href="#">Deep Drop Down 2</a></li>
+                      <li><a href="#">Deep Drop Down 3</a></li>
+                      <li><a href="#">Deep Drop Down 4</a></li>
+                      <li><a href="#">Deep Drop Down 5</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Drop Down 2</a></li>
+                  <li><a href="#">Drop Down 3</a></li>
+                  <li><a href="#">Drop Down 4</a></li>
+                </ul>
+              </li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+            <i className="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+          <a href="/courses" className="get-started-btn">Get Started</a>
+        </div>
+      </header>
+
+      <div id="main">
+        <div className="breadcrumbs" data-aos="fade-in">
+          <div className="container">
+            <h2>About Us</h2>
+            <p>Est dolorum ut non facere possimus quibusdam eligendi voluptatem. Quia id aut similique quia voluptas sit quaerat debitis. Rerum omnis ipsam aperiam consequatur laboriosam nemo harum praesentium.</p>
+          </div>
+        </div>
+
+        <section id="about" className="about">
+          <div className="container" data-aos="fade-up">
+            <div className="row">
+              <div className="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+                <img src="./src/assets/img/about.jpg" className="img-fluid" alt="ABC" />
+              </div>
+              <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+                <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+                <p className="fst-italic">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <ul>
+                  <li><i className="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                  <li><i className="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                  <li><i className="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                </ul>
+                <p>
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="counts" className="counts section-bg">
+          <div className="container">
+            <div className="row counters">
+              <div className="col-lg-3 col-6 text-center">
+                <span data-purecounter-start="0" data-purecounter-end="1232" data-purecounter-duration="1" className="purecounter"></span>
+                <p>Students</p>
+              </div>
+              <div className="col-lg-3 col-6 text-center">
+                <span data-purecounter-start="0" data-purecounter-end="64" data-purecounter-duration="1" className="purecounter"></span>
+                <p>Courses</p>
+              </div>
+              <div className="col-lg-3 col-6 text-center">
+                <span data-purecounter-start="0" data-purecounter-end="42" data-purecounter-duration="1" className="purecounter"></span>
+                <p>Events</p>
+              </div>
+              <div className="col-lg-3 col-6 text-center">
+                <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" className="purecounter"></span>
+                <p>Trainers</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="testimonials" className="testimonials">
+          <div className="container" data-aos="fade-up">
+            <div className="section-title">
+              <h2>Testimonials</h2>
+              <p>What are they saying</p>
+            </div>
+            <div className="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+              <div className="swiper-wrapper">
+                {/* Testimonial slides */}
+              </div>
+              <div className="swiper-pagination"></div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <footer id="footer">
+        <div className="footer-top">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-md-6 footer-contact">
+                <h3>Mentor</h3>
+                <p>
+                  A108 Adam Street <br />
+                  New York, NY 535022<br />
+                  United States <br /><br />
+                  <strong>Phone:</strong> +1 5589 55488 55<br />
+                  <strong>Email:</strong> info@example.com<br />
+                </p>
+              </div>
+              <div className="col-lg-2 col-md-6 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                  <li><i className="bx bx-chevron-right"></i> <a href="/">Home</a></li>
+                  <li><i className="bx bx-chevron-right"></i> <a href="/about">About us</a></li>
+                  <li><i className="bx bx-chevron-right"></i> <a href="/services">Services</a></li>
+                  <li><i className="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                  <li><i className="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                </ul>
+              </div>
+              <div className="col-lg-3 col-md-6 footer-links">
+                <h4>Our Services</h4>
+                <ul>
+                  <li><i className="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+                  <li><i className="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+                  <li><i className="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+                  <li><i className="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+                  <li><i className="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                </ul>
+              </div>
+              <div className="col-lg-4 col-md-6 footer-newsletter">
+                <h4>Join Our Newsletter</h4>
+                <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                <form action="" method="post">
+                  <input type="email" name="email" /><input type="submit" value="Subscribe" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container d-md-flex py-4">
+          <div className="me-md-auto text-center text-md-start">
+            <div className="copyright">
+              &copy; Copyright <strong><span>Mentor</span></strong>. All Rights Reserved
+            </div>
+            <div className="credits">
+              Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+          </div>
+          <div className="social-links text-center text-md-right pt-3 pt-md-0">
+            <a href="#" className="twitter"><i className="bx bxl-twitter"></i></a>
+            <a href="#" className="facebook"><i className="bx bxl-facebook"></i></a>
+            <a href="#" className="instagram"><i className="bx bxl-instagram"></i></a>
+            <a href="#" className="google-plus"><i className="bx bxl-skype"></i></a>
+            <a href="#" className="linkedin"><i className="bx bxl-linkedin"></i></a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default AboutPage;
