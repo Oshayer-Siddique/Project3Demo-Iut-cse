b20b384f1f9a9c24c3561162d746aa7490a9bd30
Oshayer-Siddique <65402474+Oshayer-Siddique@users.noreply.github.com>
2023-10-27 17:56:41 +0600

    asda

--- Backend/app.js
+++ Backend/app.js
@@ -31,7 +31,9 @@ mongoose
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin : "http://localhost:3000",
+}));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
